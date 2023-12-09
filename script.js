087628ef0742f386f4e2a5712048578ce28810e4
yusuf <kebbeyusuf@gmail.com>
2023-12-10 02:34:05 +0300

    localstorage implementaion

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ function imageMode(color) {
 function darkMode() {
   nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
   textBox.style.backgroundColor = 'rgb(255 255 255 %50)';
+  textBox.style.color = 'white';
   toggleIcon.children[0].textContent = 'Dark Mode';
   toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
 
@@ -34,16 +35,27 @@ function lightMode() {
 // Event listener
 
 function switchTheme(event) {
-
   if (event.target.checked) {
     document.documentElement.setAttribute('data-theme', 'dark')
+    localStorage.setItem('theme', 'dark');
     darkMode()
   } else {
     document.documentElement.setAttribute('data-theme', 'light')
+    localStorage.setItem('theme', 'light')
     lightMode()
   }
-
 }
+//check Local storage for theme 
 
-toggleSwitch.addEventListener('change', switchTheme)
+toggleSwitch.addEventListener('change', switchTheme);
 
+// Check Local Storage For Theme
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+  document.documentElement.setAttribute('data-theme', currentTheme);
+
+  if (currentTheme === 'dark') {
+    toggleSwitch.checked = true;
+    darkMode();
+  }
+}
