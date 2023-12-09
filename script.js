8e51807c13765b6d3343eec4805a2c434c6e74a3
yusuf <kebbeyusuf@gmail.com>
2023-12-10 01:23:59 +0300

    toggle functionallity implemented

---
+++ script.js
@@ -0,0 +1,16 @@
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+
+// Event listener
+
+function switchTheme(event) {
+
+  if (event.target.checked) {
+    document.documentElement.setAttribute('data-theme', 'dark')
+  } else {
+    document.documentElement.setAttribute('data-theme', 'light')
+  }
+
+}
+
+toggleSwitch.addEventListener('change', switchTheme)
+
